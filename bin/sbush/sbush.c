997d075eaf7759fbde523611c982ed5f0e6f41ea
Atif Ahangar <atifahangar@Atifs-MacBook-Pro.local>
2017-09-01 12:19:03 -0400

    Adding strcmp

--- bin/sbush/sbush.c
+++ bin/sbush/sbush.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
+#include <unistd.h>
+
+#define MAX_INPUT 512
+
+char scmd[512];
+char tpath[512];
+int err;
+
+int strcmp(const char * str1, const char * str2);
 
 int main(int argc, char* argv[]) {
-  puts("sbush> ");
-  return 0;
+  for(;;) {
+    puts("sbush> ");
+    gets(scmd);
+    if (~strcmp(scmd, "exit")) {
+        return 0;
+    }
+    else if (~strcmp(scmd, "pwd")) {
+        getcwd(tpath, (size_t) MAX_INPUT);
+        puts(tpath);
+    }
+    else if (scmd[0] == 'c' && scmd[1] == 'd') {
+        chdir(scmd+3);
+    }
+  }
+}
+
+int strcmp(const char * str1, const char * str2) {
+    char cmp1 = 1, cmp2 = 1;
+    int result = 1;
+    int idx = 0;
+
+    //TODO: change to do while
+    while (cmp1 != '\0' && cmp2 != '\0' && result != 0) {
+        cmp1 = str1[idx];
+        cmp2 = str2[idx];
+        result = (cmp1 == cmp2);
+
+        if (cmp1 == '\0' && cmp2 != '\0') result = 0;
+        if (cmp2 == '\0' && cmp1 != '\0') result = 0;
+
+        idx++;
+    }
+
+    return ~result;
+
 }
--- include/stdio.h
+++ include/stdio.h
@@ -6,6 +6,7 @@ static const int EOF = -1;
 int putchar(int c);
 int puts(const char *s);
 int printf(const char *format, ...);
+int strcmp(const char * str1, const char * str2);
 
 char *readline(const char *prompt);
 
