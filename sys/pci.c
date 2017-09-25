d5e435fb7ecd8e8c3d147ed092c214ed933e172b
vishal jasrotia <vishal.singh8989@gmail.com>
2017-09-25 02:53:29 -0400

    address corrected

--- sys/pci.c
+++ sys/pci.c
@@ -67,97 +67,45 @@ uint16_t pci_read(uint8_t bus, uint8_t slot,
     return (tmp);
  }
 
-
 void get_device_config(int bus, int dev, int func) {
 
-	int base_class;
-	int sub_class;
-    //int sec_bus;
+    int base_class;
+    int sub_class;
+//    int sec_bus;
 
     base_class = pci_read(bus, dev, func, PCI_CLASS_OFFSET) >> 8;
     sub_class  = pci_read(bus, dev, func, PCI_SUB_CLASS_OFFSET) & 0xff;
 
     //kprintf("CLASS: %x, SUB_CLASS: %x\n", base_class, sub_class);
 
-    if(base_class == AHCI_CLASS && sub_class == AHCI_SUBCLASS) {
+    if(base_class == 0x01 && sub_class == 0x06) {
         kprintf("AHCI Found!!\n");
-//        volatile uint64_t bar_p0 = pci_read(bus, dev, func, 0x25);
-//        volatile uint64_t bar_p1 = pci_read(bus, dev, func, 0x27);
-//        volatile uint64_t bar_p2 = pci_read(bus, dev, func, 0x21);
-//        volatile uint64_t bar_p3 = pci_read(bus, dev, func, 0x23);
-
-
-		volatile uint64_t bar_p0 = pci_read(bus, dev, func, 0x25);
-		volatile uint64_t bar_p1 = pci_read(bus, dev, func, 0x27);
-		volatile uint64_t bar_p2 = pci_read(bus, dev, func, 0x21);
-		volatile uint64_t bar_p3 = pci_read(bus, dev, func, 0x23);
-        volatile uint64_t bar4 = (bar_p2 | (bar_p3 << 16)) & 0xfffffff0;
-        volatile uint64_t bar5 = (bar_p0 | (bar_p1 << 16)) & 0xffffffff;
-        //volatile uint64_t bar = bar4 + (bar5 << 32);
-        //volatile uint64_t oneGB = 1024*1024*1024;
-        //volatile uint64_t bar =  bar5 >> 16;
-        //volatile uint64_t bar = bar5;
         uint32_t address = (uint32_t)((bus << 16) | (dev << 11) |
                            (func << 8) | 0x24 | ((uint32_t)0x80000000));
+        kprintf("address %p\n", address);
         outl(PCI_CONFIG_ADDR, address);
         volatile uint64_t bar = inl(PCI_CONFIG_DATA) & 0xffffffff;
-        kprintf("BAR: %p\n", bar);
+        kprintf("BAR before Remap: %p\n", bar);
 
         bar = 0xffffffff;
         outl(PCI_CONFIG_ADDR, address);
         outl(PCI_CONFIG_DATA, bar);
         bar = inl(PCI_CONFIG_DATA) & 0xffffffff;
         kprintf("BAR Range: %p\n", bar);
-        //volatile uint64_t oneGB = 1024*1024*1024;
-        //volatile uint64_t bar =  bar5 >> 16;
-        //volatile uint64_t bar = bar5;
-        //uint32_t address = (uint32_t)((bus << 16) | (dev << 11) |
-         //                  (func << 8) | 0x24 | ((uint32_t)0x80000000));
-        //outl(PCI_CONFIG_ADDR, address);
-        //volatile uint64_t bar = (bar_p4 & 0xFFFFFFF0) + ((bar_p5 & 0xFFFFFFFF) << 32);//inl(PCI_CONFIG_DATA) & 0xffffffff;
-        //kprintf("address: %p\n",address);
-        kprintf("BAR[0]: %p\n",bar_p0);
-        kprintf("BAR[1]: %p\n",bar_p1);
-        kprintf("BAR[2]: %p\n",bar_p2);
-        kprintf("BAR[3]: %p\n",bar_p3);
-        //kprintf("BAR[4]: %p\n",bar_p4);
-        //kprintf("BAR[5]: %p\n",bar_p5);
-        kprintf("BAR: %p\n", bar);
 
-        //bar = 0xffffffff;
-        //outl(PCI_CONFIG_ADDR, address);
-        //outl(PCI_CONFIG_DATA, bar);
-//        bar = inl(PCI_CONFIG_DATA) & 0xffffffff;
-//        kprintf("BAR Range: %p\n", bar);
-////
-//        bar = 0x10010000;
-//        outl(PCI_CONFIG_ADDR, address);
-//        outl(PCI_CONFIG_DATA, bar);
-//        bar = inl(PCI_CONFIG_DATA) & 0xffffffff;
-//        kprintf("BAR: %p\n", bar);
-//
-//        //bar_p0 = pci_read(bus, dev, func, 0x25);
-//        //bar_p1 = pci_read(bus, dev, func, 0x27);
-//        //bar5 = (bar_p0 | (bar_p1 << 16)) & 0xffffffff;
-        //abar = (uint64_t *) bar_p4;
-        //kprintf("bar4: %p, bar5: %p, bar: %p\n", bar4, bar5, bar);
-        //kprintf("ABAR: %p\n", abar);
-
-
-        abar = (uint64_t *) bar4;
-          //kprintf("bar4: %p, bar5: %p, bar: %p\n", bar4, bar5, bar);
-          kprintf("ABAR: %p\n", abar);
-
-          abar = (uint64_t *) bar5;
-            //kprintf("bar4: %p, bar5: %p, bar: %p\n", bar4, bar5, bar);
-            kprintf("ABAR: %p\n", abar);
-
+        //bar = 0x10010000;
+        bar = 0x30000000;
+        outl(PCI_CONFIG_ADDR, address);
+        outl(PCI_CONFIG_DATA, bar);
+        bar = inl(PCI_CONFIG_DATA) & 0xffffffff;
         abar = (uint64_t *) bar;
-         //kprintf("bar4: %p, bar5: %p, bar: %p\n", bar4, bar5, bar);
-         kprintf("ABAR: %p\n", abar);
+        kprintf("ABAR: %p\n", abar);
     }
 
-//PCI-PCI bridge cdetect and config goes here. heck base and sun coe for PCI bridge.
+//    if((base_class == 0x06) && (sub_class == 0x04)) {
+//        sec_bus = pci_read(bus, dev, func, 0x19) >> 8;
+//        pci_scan_bus(sec_bus);
+//    }
 }
 
 int is_valid_device(uint8_t bus, uint8_t device, uint8_t function){
@@ -182,10 +130,10 @@ void get_bar(uint8_t bus, uint8_t device,uint8_t function){
 
 
 
-    			kprintf("bar offset %d :  %p\n",bar_num,bar_offset);
+    			//kprintf("bar offset %d :  %p\n",bar_num,bar_offset);
     			uint8_t bar_type  = (pci_read(bus, device, function, bar_offset) & 0x1) ? BAR_IO : BAR_MEM; //first is always 0 for mem
     			if(bar_type == BAR_MEM){
-				kprintf("BAR is mem.\n");
+//				kprintf("BAR is mem.\n");
 				//check bit size
 				switch ((pci_read(bus, device, function, bar_offset) >> 1) & 0x3){
 				//
@@ -193,25 +141,14 @@ void get_bar(uint8_t bus, uint8_t device,uint8_t function){
 					{
 							uint64_t bar_value = pci_read(bus, device, function ,bar_offset);
 							if (bar_value !=0x00){ // 0 not valid
-
+								uint64_t* address = (uint64_t*)(bar_value & ~0x3);
 								//abar = (uint32_t *)bar_value;
-								kprintf("BAR value is %p\n ", bar_value);
-								void *addr = (void*)bar_value;
+								kprintf("BAR value is %p, %p\n ", bar_value, address);
 
-																if(addr){}
-																kprintf("BAR value is %p. add %p \n", bar_value , *(uint32_t*)addr);
+
+
 							}else{
-								//kprintf ("Device is valid\n");
-								void *addr = (void*)bar_value;
 
-								if(addr){}
-								kprintf("BAR value is %p. add %p \n", bar_value , *(uint32_t*)addr);
-								//						 int low_bit = get_number_of_lowest_set_bit(bar_value);
-								//						 kprintf ("Low bit is %d\n", low_bit);
-								//						 if ((get_number_of_highest_set_bit(bar_value) != 31) || (low_bit> 31) || (low_bit <4)){
-								//							 kprintf ("Incorrect bar address\n", bar_num); continue; }
-								//
-								//						 kprintf ("BAR %d has %p address. \n", bar_num, low_bit);
 							}
 							break;
 					}
@@ -287,17 +224,17 @@ void scan_device(uint8_t bus, uint8_t device) {
      for(uint8_t function = 0;  function <  num_of_function ;function++){
     	 	  uint16_t device_id = pci_read(bus, device, function, PCI_DEVICE_ID_OFFSET);
     	 	  if(device_id == PCI_INVALID_DID){
-    	 		  	 break;
+    	 		  	 continue;
     	 	  }
     	      uint16_t vendor_id = pci_read(bus, device, function, PCI_VENDOR_ID_OFFSET);
     	      kprintf("PCI bus %p : Vendor id : %p , Device id : %p. Function : %p \n",bus, vendor_id, device_id, function);
 
     	    	  get_bar(bus, device, function);
 
-
+    	      //get_device_config(bus, device, function);
     	      }
 
-    	      //get_device_config(bus, device, function);
+
     	     // kprintf("value at bar %");
 
 
