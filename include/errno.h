753b5151681427e1ee73cd973f47aa835db93da5
vishal jasrotia <vishal.singh8989@gmail.com>
2017-11-30 18:37:01 -0500

    Stable after master pull.

--- include/errno.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ERROR_H
-#define _ERROR_H
-
-#include<sys/defs.h>
-
-
-
-#endif
--- include/stdint.h
+++
@@ -1,8 +0,0 @@
-#ifndef _STDINT_H
-#define _STDINT_H
-
-#include<sys/defs.h>
-
-typedef uint64_t uintptr_t;
-
-#endif
