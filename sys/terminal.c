c612c0f0393cea5a26bb198fde3a5829e67f6cdb
vishal jasrotia <vishal.singh8989@gmail.com>
2017-12-08 18:18:48 -0500

    term fix.

--- sys/terminal.c
+++ sys/terminal.c
@@ -1,43 +1,36 @@
 #include <sys/defs.h>
 #include <sys/kprintf.h>
 #include <sys/process.h>
-#include <sys/utils.h>
 
-char term_buf[64];
+char term_buf[128];
 char input_ready = 0;
 volatile uint64_t term_idx = 0;
 volatile uint64_t read_idx = 0;
 
-int termidx(){
-	return term_idx;
-}
 void upd_term_buf(char c)
 {
     //TODO: don't use static storage, use kmalloc for each process.
 
-    term_buf[term_idx % 64] = c;
 
-    if(c == BACKSPACE) {
-    		//if(term_idx >= 0){
-    			term_idx--;
-    	//	}
-       //kprintf("\nBInput Ready! in Buf : %s\n", term_buf);
+
+    if(c == BACKSPACE && (term_buf[term_idx-1 % 128] != '\n')) {
+        term_buf[term_idx % 128] = '\n';
+        term_idx--;
+
+        //read_idx = term_idx;
     }
     else if(c == ENTER) {
+    term_buf[term_idx % 128] = c;
        input_ready = 1;
        term_idx++;
-
-       //memset(term_buf, '\0', sizeof(term_buf));
-       //memset(term_buf , '\0', 64);
-       //kprintf("\nEInput Ready! in Buf : %s\n", term_buf);
+       //kprintf("\nInput Ready! in Buf\n");
     }
     else {
+        term_buf[term_idx % 128] = c;
        term_idx++;
-       //kprintf("\nNInput Ready! in Buf : %s\n", term_buf);
     }
 
     pchar(c);
-    //kprintf("term_idx : %s\n",term_buf);
 }
 
 void term_read(uint64_t addr, uint64_t size)
@@ -53,9 +46,13 @@ void term_read(uint64_t addr, uint64_t size)
 
     for(int i = 0; i < size; i++) {
         //*(addri+i) = term_buf[--term_idx];
-        c = term_buf[read_idx++ % 64];
+
+        c = term_buf[read_idx++ % 128];
         *(addri+i) = c;
-        if(c == ENTER) input_ready = 0;
+        if(c == ENTER) {
+            input_ready = 0;
+
+        }
     }
 }
 
